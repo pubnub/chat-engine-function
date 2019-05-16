361b442f8141d715c42126a4328ecf60e3c1bcfe
Jeff Moore <43356697+MessagingMoore@users.noreply.github.com>
2019-05-16 16:50:46 -0700

    Remove unncessary files (#9)

--- src/helpers/applySignature.js
+++
@@ -1,5 +0,0 @@
-import queryStringCodec from "codec/query_string";
-
-export default (options, signature) => {
-  return queryStringCodec.stringify(Object.assign(options, { signature }));
-};
--- src/helpers/encodeURI.js
+++
@@ -1,6 +0,0 @@
-export default s => {
-  return encodeURIComponent(s).replace(
-    /[!~*'()]/g,
-    c => `%${c.charCodeAt(0).toString(16)}`
-  );
-};
